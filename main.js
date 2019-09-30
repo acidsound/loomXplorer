fb1a3eb3c68f5155d3745f28bc25f00d6dc1da97
JaehoLee <spectrick@gmail.com>
2019-09-30 23:35:15 +0900

    add date-fn packages, and time information

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-// for async/await 
+// for async/await
 import 'regenerator-runtime/runtime'
+import { formatDistance } from 'date-fns'
 
 const cmds = {
   "endpoint": "https://plasma.dappchains.com/rpc",
@@ -29,6 +30,7 @@ const onBlockClickHandler = async e => {
   document.querySelector("#blockDetail").textContent = JSON.stringify(block, null, 2)
 }
 
+const getSinceFrom = since => formatDistance(Date.parse(since), new Date())
 const updateHashLists = ({blockMetas})=> {
   const list = document.querySelector("#blockMeta")
   document.querySelectorAll("#blockMeta .item").forEach(o=>o.remove())
@@ -39,20 +41,24 @@ const updateHashLists = ({blockMetas})=> {
     node.setAttribute("data-id", v.header.height)
     node.querySelector('.head').textContent = v.header.height
     node.querySelector('.desc>.validator>.link').textContent = `loom${v.header.proposer_address}`
+    const since = node.querySelector('.desc>.since')
+    since.textContent = getSinceFrom(v.header.time)
+    since.setAttribute('data-since', getSinceFrom(v.header.time))
     node.addEventListener("click", onBlockClickHandler)
     list.append(node)
   })
 }
 const initApps = async ()=>{
   console.log(+(new Date()), "init Apps", )
-  bs.status = await getChainStatus()
-  console.log("block status updated", bs)
-  if (bs.status["sync_info"] && bs.status["sync_info"]["latest_block_height"]) {
-    let bh = +bs.status["sync_info"]["latest_block_height"]
-    console.log(bh)
-    bs.blocks = await getBlocks({from: bh-9, to: +bh})
-    updateHashLists({blockMetas: bs.blocks.block_metas})
-  }
+  let handle = setInterval(async ()=>{
+    bs.status = await getChainStatus()
+    console.log("block status updated", bs)
+    if (bs.status["sync_info"] && bs.status["sync_info"]["latest_block_height"]) {
+        let bh = +bs.status["sync_info"]["latest_block_height"]
+        bs.blocks = await getBlocks({from: bh-9, to: +bh})
+        updateHashLists({blockMetas: bs.blocks.block_metas})
+    }
+  }, 1000)
 }
 
 document.addEventListener('DOMContentLoaded', initApps);
